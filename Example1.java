d3772148a7807fa758c0191ed186994249f973aa
susmitha <susmitha.t9642@gmail.com>
2024-08-06 17:01:56 +0530

    commit completed

--- Example1.java
+++ Example1.java
@@ -1,6 +1,6 @@
 public class Example1{
     public static void main(String[] args){
-        String name = "Smith";
+        String name = "Johns";
         System.out.println("hello world!");
     }
 }
