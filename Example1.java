20c506869a17d473cf6cf1436832d0d321f16056
susmitha <susmitha.t9642@gmail.com>
2024-08-06 17:20:50 +0530

    Example1.java

--- Example1.java
+++ Example1.java
@@ -1,7 +1,7 @@
 public class Example1{
     public static void main(String[] args){
-       int id = 1;
-       int age = 1;
+       float sal = 10000;
+       int range = 1;
         System.out.println("hello world!");
     }
 }
